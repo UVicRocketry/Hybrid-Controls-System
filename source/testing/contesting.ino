93e78a4c26e157b8271c22bd31489a77d10e471a
Ben Say <benbsay@gmail.com>
2021-03-03 21:05:42 -0800

    added MEV_closed

--- source/testing/contesting.ino
+++ source/testing/contesting.ino
@@ -11,11 +11,19 @@ void loop(){
     if(Serial.available()){
         data = Serial.readString();
 
-        if(data == "MEV open"){
+        if(data == "MEV_open"){
             digitalWrite(13, HIGH);
             delay(1000);
             digitalWrite(13, LOW);
         }
+
+        if(data == "MEV_closed"){
+            digitalWrite(13, HIGH);
+            delay(1000);
+            digitalWrite(13, LOW);
+        }
+
+        
 
     }
 
