fe9378da279470934f80d54f7e7794531dd8808d
klemie <lemieuxkristopher@gmail.com>
2021-03-03 21:02:54 -0800

    send MEV_open test

--- source/testing/testingdata.py
+++ source/testing/testingdata.py
@@ -1,7 +1,10 @@
 from serial import Serial
 
 arduino = Serial("COM5", 115200)
-arduino.open()
+# arduino.open()
 
 def write_to_serial(command):
     arduino.writelines(command.encode("utf-8"))
+    print(command)
+
+write_to_serial("MEV_open")
