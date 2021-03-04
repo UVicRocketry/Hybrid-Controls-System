1569b3185a5158e6313a3d43fb75349f0da6c117
Ben Say <benbsay@gmail.com>
2021-03-03 21:38:42 -0800

    made it better

--- source/testing/contesting.ino
+++ source/testing/contesting.ino
@@ -12,11 +12,19 @@ void loop(){
         data = Serial.readString();
 
         /*
-        first set of flashes indicates the target. second set indicates setting.
-
-        f
+        First set of flashes indicates the target. Second set of flashes indicates setting.
 
         1 flash means setting closed, 2 flashes means setting open.
+
+        First set:
+            0 flashes
+            1 flashes - MEV
+            2 flashes - N2OV
+            3 flashes - N2O
+            4 flashes - N2
+            5 flashes - NCV
+            6 flashes - RV
+            7 flashes - N2V
         */
 
         //igniter control flow here
@@ -30,6 +38,8 @@ void loop(){
 
         else if(data == "MEV_open"){ 
             digitalWrite(13, HIGH);
+            delay(500);
+            digitalWrite(13, LOW);
             delay(500);
             digitalWrite(13, HIGH);
             delay(500);
