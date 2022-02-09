c79777e0c8849bd63c3e2413cb5d0df9503b3ae5
klemie <lemieuxkristopher@gmail.com>
2022-02-08 23:37:21 -0800

    Called read_from_serial to echo commands back

--- source/controller.py
+++ source/controller.py
@@ -8,9 +8,11 @@ can read and send it through client.py.
 #assume everything is a dictionary
 # 
 
-import client
 import threading
+
 from serial import Serial
+
+import client
 
 baud_rate = 115200          # In the arduino .ino file, Serial.begin(baud_rate)
 serial_port = "dev/ttyUSB0" # Something similar to this. It will depend what usb port the arduino is connected to
@@ -60,7 +62,10 @@ class Receiver:
         :return: Nothing
         """
 
-        while True:
+        while True: #TODO: add limit switch fail check. Ie if limit switch is not send back in X amount of time go into fail safe mode
+            
+            if ser.in_waiting:
+                self.ctrl.read_from_serial() # updates if there is a response from the Controller. Make sure to test if this actually works in testing
 
             try:
                 self.client.receive_states()  # will hang up on this line until instructions are received
@@ -77,7 +82,7 @@ class Receiver:
                     self.fail_state('Connection Ended')
                     return
 
-                self.ctrl.write_to_serial(f'{param} {state}')  # send the command to our controller
+                self.ctrl.write_to_serial(f'{param} {state}')  # send the command to the arduino controller
 
     def fail_state(self, error):
         """
