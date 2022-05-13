76e395d46d40d5836cbc2c31152172a7fe1ac89e
ByonicTao <35857151+ByonicTao@users.noreply.github.com>
2022-05-13 14:47:44 -0700

    started serial comm

--- source/MissionControlBox/PDPMCBFirmware/PDPMCBFirmware.ino
+++ source/MissionControlBox/PDPMCBFirmware/PDPMCBFirmware.ino
@@ -22,11 +22,27 @@
 #define clockPin 5
 #define dataPin 6
 
+//valve aliases
+String V1 = "V1";
+String V2 = "V2";
+String V3 = "V3";
+String V4 = "V4";
+String V5 = "V5";
+String V6 = "V6";
+String V7 = "V7";
+String V8 = "V8";
+String V9 = "V9";
+String V10 = "V10";
+
+String * valveList[10];
+
 byte ledTop = 0;    // Variable to hold the pattern of which LEDs are currently turned on or off
 byte ledBot = 0;
 
 void setup() {
   Serial.begin(9600);
+  while (!Serial){};
+  
   pinMode(R1, OUTPUT);
   pinMode(R2, OUTPUT);
   pinMode(R3, OUTPUT);
@@ -38,28 +54,37 @@ void setup() {
   pinMode(latchPin, OUTPUT);
   pinMode(dataPin, OUTPUT);  
   pinMode(clockPin, OUTPUT);
+
+  valveList[0] = & V1;
+  valveList[1] = & V2;
+  valveList[2] = & V3;
+  valveList[3] = & V4;
+  valveList[4] = & V5;
+  valveList[5] = & V6;
+  valveList[6] = & V7;
+  valveList[7] = & V8;
+  valveList[8] = & V9;
+  valveList[9] = & V10;
+  
 }//setup
 
 void loop() {
-    //TODO
-    //Write new LED logic
-
     ledTop = 0;
     ledBot = 0;
     String switchStr = "SS,";
     digitalWrite(R1, HIGH);
-    switchStr += valveSwitchRead("V1", C1);
-    switchStr += valveSwitchRead("V2", C2);
-    switchStr += valveSwitchRead("V3", C3);
-    switchStr += valveSwitchRead("V4", C4);
-    switchStr += valveSwitchRead("V5", C5);
+    switchStr += valveSwitchRead(V1, C1);
+    switchStr += valveSwitchRead(V2, C2);
+    switchStr += valveSwitchRead(V3, C3);
+    switchStr += valveSwitchRead(V4, C4);
+    switchStr += valveSwitchRead(V5, C5);
     digitalWrite(R1, LOW);
     digitalWrite(R2, HIGH);
-    switchStr += valveSwitchRead("V6", C1);
-    switchStr += valveSwitchRead("V7", C2);
-    switchStr += valveSwitchRead("V8", C3);
-    switchStr += valveSwitchRead("V9", C4);
-    switchStr += valveSwitchRead("V10", C5);
+    switchStr += valveSwitchRead(V6, C1);
+    switchStr += valveSwitchRead(V7, C2);
+    switchStr += valveSwitchRead(V8, C3);
+    switchStr += valveSwitchRead(V9, C4);
+    switchStr += valveSwitchRead(V10, C5);
     digitalWrite(R2, LOW);
     
     updateShiftRegister();
@@ -81,6 +106,13 @@ void loop() {
     
     digitalWrite(R3, LOW);
     Serial.println(switchStr);
+    /*String valveState = Serial.read();
+
+    int i;
+    
+    for(i = 0; i < 10(); i++;){
+      valveParse(valveState, *valveList[i]);
+    }//for*/
     
 }//loop
 
@@ -100,6 +132,24 @@ String valveSwitchRead(String valveName, int valveCol){
   
   return valveStatus;
 }//valveSwitchRead
+
+void valveParse (String input, String valveName){
+  if(input.substring(0, 2) == "VS"){
+     int index = input.indexOf(valveName + ",");
+     if(index != -1){
+      int end = input.indexOf(",", valveName.length() + 1);
+      String value = input.substring(index + valveName.length() + 1, end);
+      if(value == "O"){
+        if(valveName.substring(1).toInt() > 8){
+          bitSet(ledBot, valveName.substring(1).toInt() - 9);
+        } else {
+          bitSet(ledTop, valveName.substring(1).toInt() - 1 );
+        }//ifelse
+      }//if
+     }//if
+     
+  }//if
+}//valveParse
 
 /*
  * updateShiftRegister() - This function sets the latchPin to low, then calls the Arduino function 'shiftOut' to shift out contents of variable 'leds' in the shift register before putting the 'latchPin' high again.
